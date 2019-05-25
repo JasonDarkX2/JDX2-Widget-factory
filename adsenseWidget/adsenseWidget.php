845b4ff743b2d71f73f62bc0957e15dcddabff09
Jason Dark <JasonDarkX2@users.noreply.github.com>
2019-05-25 19:48:23 -0400

    implemented some new  widgets

--- adsenseWidget/adsenseWidget.php
+++ adsenseWidget/adsenseWidget.php
@@ -62,7 +62,8 @@ case 'White box':
         $title= $instance['title'];
         $text= $instance['content'];
         $adClient= $instance['adClient'];
-        $adSlot= $instance['adSlot']
+        $adSlot= $instance['adSlot'];
+        $testData= $instance['testData'];
 ?>
 <label for="options">Ad Header:</label>
  <select name="<?php echo$this->get_field_name( 'adHeader'); ?>" style="width:100%;">
@@ -122,6 +123,18 @@ case 'White box':
     }
 } ?>
     </select>
+        <br/>
+        <label for="options"> Test Data:</label>
+        <br/>
+        <span style=" margin-top:2px; display:flex; flex-direction:row;">
+            <?php if($instance['testData']=="on") {
+            echo '<input type="radio" name="' . $this->get_field_name('testData') . '" value="on" checked>On<br>';
+            echo '<input type="radio" name="' . $this->get_field_name('testData') . '" value="off">Off<br>';
+        }else{
+                echo'<input type="radio" name="' .  $this->get_field_name( 'testData') . '"  value="on">On<br>';
+                echo '<input type="radio" name="' . $this->get_field_name( 'testData')  . '"  value="off" checked>Off<br>';
+            }?>
+        </span>
 <?php }
 public function update($new_instance, $old_instance) {
         $instance = array();
@@ -131,6 +144,7 @@ public function update($new_instance, $old_instance) {
         $instance['adSize'] = ( ! empty( $new_instance['adSize'] ) ) ? strip_tags( $new_instance['adSize'] ) : '';
          $instance['adHeader'] = ( ! empty( $new_instance['adHeader'] ) ) ? strip_tags( $new_instance['adHeader'] ) : '';
          $instance['adWrap'] = ( ! empty( $new_instance['adWrap'] ) ) ? strip_tags( $new_instance['adWrap'] ) : '';
+        $instance['testData'] = ( ! empty( $new_instance['testData'] ) ) ? strip_tags( $new_instance['testData'] ) : '';
         return $instance;
 }
 function get_size($instance){
@@ -185,7 +199,9 @@ function create_ad($instance){?>
     <ins class="adsbygoogle"
          style="display:inline-block; <?php echo $this->get_size($instance['adSize']); ?>"
          data-ad-client="<?php echo $instance['adClient']; ?>"
-         data-ad-slot="<?php echo $instance['adSlot']; ?>"</ins>
+         data-ad-slot="<?php echo $instance['adSlot']; ?>"
+         data-adtest="<?php echo $instance['testData']; ?>"
+    </ins>
          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
 <?php }
 }
---
+++ popularPost/popularPost.php
@@ -0,0 +1,44 @@
+<?php 
+class popularPost_widget extends WP_Widget {
+
+function __construct() {
+parent::__construct(
+// Base ID of your widget
+'JDX2-popularpost', 
+
+// Widget name will appear in UI
+__('Popular Posts(powered by JDX2)', 'JDX2_popularp_widget_domain'), 
+
+// Widget description
+array( 'description' => __( 'Simply showing the popular posts widget \(^.^)/ ',  
+        'JDX2_popularp_widget_domain' ), ) 
+);
+}
+// Creating widget front-end
+// This is where the action happens
+public function widget( $args, $instance ) {
+    
+    $title="Popular Posts";
+    echo $args['before_widget'];
+    echo $args['before_title'] . $title . $args['after_title'];
+$popularpost = new WP_Query( array( 'posts_per_page' => 5, 'meta_key' => 'post views', 
+    'orderby' => 'meta_value_num', 'order' => 'DESC'  ) );
+echo'<ul class="popularPosts">';
+
+while ( $popularpost->have_posts() ) : $popularpost->the_post();?>
+<li><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>" ><?php 
+//$attr = array( 'alt' =>  get_the_title());
+//echo get_the_post_thumbnail($post->ID,'postThumbNails',$attr);
+the_title();
+?> </a></li><?php endwhile;
+echo"</ul>";
+echo $args['after_widget'];
+}
+}// Class wpb_widget ends here
+
+// Register and load the widget
+function popular_load_widget() {
+	register_widget( 'popularPost_widget' );
+}
+add_action( 'widgets_init', 'popular_load_widget' );
+?>
